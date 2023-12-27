e759ecf104f6b9d4aa0948fe23b34d95c26a00e0
JianYiEn <c44111099@gs.ncku.edu.tw>
2023-12-27 10:40:28 +0800

    注音音檔

--- 知識王/Assets/Scripts/MainMenu.cs
+++ 知識王/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("SelectMenu");
     }
     public void Quit()
     {
@@ -15,6 +15,6 @@ public class MainMenu : MonoBehaviour
     }
     public void Setting()
     {
-        
+        SceneManager.LoadScene("SettingMenu");
     }
 }
